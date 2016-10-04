e7ed97b180570041e117d148d5f80c2c2dc56046
Ugo Landini <ugo.landini@gmail.com>
2016-10-04 12:58:55 +0200

    Added Spring-boot example and changed project directory for each technology

---
+++ spring-boot/echo/src/main/java/it/redhat/osd/spring/boot/EchoApplication.java
@@ -0,0 +1,12 @@
+package it.redhat.osd.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoApplication.class, args);
+	}
+}
