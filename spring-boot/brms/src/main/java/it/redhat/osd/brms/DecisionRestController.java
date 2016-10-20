d121e61d4b72ea3a24dfc47d2f4d2a207117b9f7
Andrea Leoncini <andrea.leoncini@gmail.com>
2016-10-21 00:51:23 +0200

    prima versione del micro service con brms

---
+++ spring-boot/brms/src/main/java/it/redhat/osd/brms/DecisionRestController.java
@@ -0,0 +1,37 @@
+package it.redhat.osd.brms;
+
+import it.redhat.osd.model.Info;
+import it.redhat.osd.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class DecisionRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DecisionRestController.class);
+    private final AtomicLong counter = new AtomicLong();
+    private final DecisionService decisionService;
+
+
+    @Autowired
+    public DecisionRestController(DecisionService decisionService){
+        this.decisionService = decisionService;
+    }
+
+    @RequestMapping(value="/brms/ds/info", method = RequestMethod.GET)
+    public @ResponseBody
+    Info info( ) {
+        return new Info().setCounter(counter.incrementAndGet());
+    }
+
+    @RequestMapping(value="/brms/ds/order", method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody Order order(@RequestBody Order order) {
+        Order calculatedOrder = decisionService.getResult(order);
+        return calculatedOrder;
+    }
+
+}
