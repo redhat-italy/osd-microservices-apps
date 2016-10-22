5c73ff659bd90c357e6e9a05e17ccae808486614
Ugo Landini <ugo.landini@gmail.com>
2016-10-22 10:18:20 +0200

    added intermediate versions

--- go/doccher/doccher0.go
+++ go/doccher/doccher0.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"syscall"
 )
 
 func main() {
@@ -21,36 +20,19 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		parent()
-	case "child":
-		child()
 	default:
 		panic("wat?")
 	}
 }
 
 func parent() {
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	must(cmd.Run())
-}
-
-func child() {
 	fmt.Printf("running %v as pid %d\n", os.Args[2:], os.Getpid())
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	must(syscall.Chroot("/home/doccher/"))
-	must(syscall.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	must(cmd.Run())
 }
-
 
 func must(err error) {
 	if err != nil {
